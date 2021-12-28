9d4ebcec74a70fae60db6638e994f085843ae657
Zachary Koo <koo_z@bellsouth.net>
2021-12-27 20:56:46 -0500

    uploaded file saved to state

---
+++ frontend/src/components/domain/Timesheet.js
@@ -0,0 +1,13 @@
+export default class Timesheet {
+
+    userId;
+    weekEnding;
+    billingHour;
+    compoensatedHour;
+    attachment;
+    hasAttachment;
+    submissionStatus;
+    timesheet;
+    weekEnding;
+
+ }
