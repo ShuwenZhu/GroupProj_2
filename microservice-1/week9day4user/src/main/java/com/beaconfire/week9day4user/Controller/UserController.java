b599840f5d60995a7e7e394145f11b106653859a
shuwenzhu <shuwenzhu.cici@gmail.com>
2021-12-29 23:55:23 -0500

    bug fix

--- microservice-1/week9day4user/src/main/java/com/beaconfire/week9day4user/Controller/UserController.java
+++ microservice-1/week9day4user/src/main/java/com/beaconfire/week9day4user/Controller/UserController.java
@@ -95,18 +95,21 @@ public class UserController {
     }
     
     @PostMapping("/changeStatSet") //for hr changing user request status
-    public ResponseMsg changeTimesheetApprovalStatus(HttpServletRequest httpServletRequest)
+    public ResponseMsg changeTimesheetApprovalStatus(
+    		@RequestParam("userId")Integer userId,
+    		@RequestParam("date") String date,
+    		@RequestParam("status") String status
+    		)
     {
-    	Integer userId = Integer.parseInt(httpServletRequest.getParameter("userId"));
-    	String date = httpServletRequest.getParameter("date");
-    	String status = httpServletRequest.getParameter("status");
     	if (status.compareToIgnoreCase("Approved")==0)
 		{
+    		System.out.println("Approving userId: " + userId + " " + date);
 		  if(timesheetService.approve(userId, date, status))
 			  return new ResponseMsg("succeed");
 		}
     	else if(status.compareToIgnoreCase("Not Approved")==0)
     	{
+    		System.out.println("Rejecting userId: " + userId + " " + date);
     		if(timesheetService.reject(userId,date,status))
     			return new ResponseMsg("succeed");
     	}
@@ -123,5 +126,16 @@ public class UserController {
     public ResponseEntity<Optional<List<TimesheetRecord>>> getUserWERecord(@RequestHeader Map<String, String> headers, @RequestParam Integer userId)
     {
     	return ResponseEntity.ok(timesheetService.getRecords(userId));
+    }
+    
+    @GetMapping("/updateStats")
+    public ResponseEntity<String> updateTimesheet(
+    		@RequestParam Integer userId, 
+    		@RequestParam String weDate,
+    		@RequestParam String sbStats,
+    		@RequestParam boolean approvedFile)
+    {
+    	timesheetService.updateTimesheet(userId,weDate,sbStats,approvedFile);
+    	return ResponseEntity.ok("success");
     }
 }
